ac02f3f0d686b944347cb5018b1e2783e386d81c
dupas louis <tawairpofficiel@gmail.com>
2022-10-12 01:04:03 +0200

    First Movement Test
    Velocity issue

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Player Variables")]
+
+    public float speed = 3f;
+    private Rigidbody playerRb;
+    private PlayerMovementInput playerMovementInput;
+    private Vector2 moveInput;
+
+
+    private void Awake()
     {
-        
+        playerMovementInput = new PlayerMovementInput();
+
+        playerRb = GetComponent<Rigidbody>();
+        playerMovementInput.PlayerMap.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        PlayerMove();
+    }
+
+
+    private void OnEnable()
+    {
+        playerMovementInput.PlayerMap.Enable();   
+    }
+
+    private void OnDisable()
+    {
+        playerMovementInput.PlayerMap.Disable();
+    }
+
+
+    private void PlayerMove()
+    {
+        Vector3 playerVelocity = new Vector3(moveInput.x * speed, playerRb.velocity.y, moveInput.y * speed);
+        playerRb.velocity = transform.TransformDirection(playerVelocity);
+    }
+
+    public void OnMovement(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
     }
 }
