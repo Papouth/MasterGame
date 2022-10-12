4801e6732947ba65cff836e25f57d127e5995909
Dupas Louis <tawairpofficiel@gmail.com>
2022-10-12 15:17:38 +0200

    Fix Déplacement

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -9,34 +9,18 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 3f;
     private Rigidbody playerRb;
-    private PlayerMovementInput playerMovementInput;
     private Vector2 moveInput;
 
 
     private void Awake()
     {
-        playerMovementInput = new PlayerMovementInput();
-
         playerRb = GetComponent<Rigidbody>();
-        playerMovementInput.PlayerMap.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
     }
 
     private void FixedUpdate()
     {
         PlayerMove();
     }
-
-
-    private void OnEnable()
-    {
-        playerMovementInput.PlayerMap.Enable();   
-    }
-
-    private void OnDisable()
-    {
-        playerMovementInput.PlayerMap.Disable();
-    }
-
 
     private void PlayerMove()
     {
