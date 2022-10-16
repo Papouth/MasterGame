7af64fd1207cc26d00d8d61353c8b0fea8316ce6
OmoxLb <Lecluse.theo@gmail.com>
2022-10-16 19:29:38 +0200

    Maj Movement
    - Maj Player Input Add
    - Maj Climb Add
    - Maj Raycast Grounded -> Raycast Check
    - Maj Interaction
    - Maj Scene Main Menu

---
+++ Assets/Scripts/Player Scripts/PlayerClimb.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClimb : MonoBehaviour
+{
+    public LayerMask layerCanClimb;
+    public float rangeMaxClimb = 1f;
+
+    public float forceClimb = 1.5f;
+
+    public RaycastCheck[] raycastClimbsUp;
+    public RaycastCheck[] raycastClimbsDown;
+
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake()
+    {
+        foreach (RaycastCheck raycastCheck in raycastClimbsUp)
+        {
+            raycastCheck.layer = layerCanClimb;
+            raycastCheck.rangeMax = rangeMaxClimb;
+            raycastCheck.directionRaycast = Vector3.forward;
+        }
+        foreach (RaycastCheck raycastCheck in raycastClimbsDown)
+        {
+            raycastCheck.layer = layerCanClimb;
+            raycastCheck.rangeMax = rangeMaxClimb;
+            raycastCheck.directionRaycast = Vector3.forward;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CheckCanClimb())
+        {
+            Climb();
+        }
+    }
+
+
+    /// <summary>
+    /// Check si le joueur peut climb une surface
+    /// </summary>
+    /// <returns></returns>
+    private bool CheckCanClimb()
+    {
+        //Check raycast
+
+        int raycastUpGood = 0;
+        int raycastDownGood = 0;
+
+        foreach (RaycastCheck raycast in raycastClimbsUp)
+        {
+            if (raycast.RaycastTest()) raycastUpGood++;
+        }
+
+        foreach (RaycastCheck raycast in raycastClimbsDown)
+        {
+            if (raycast.RaycastTest()) raycastDownGood++;
+        }
+
+
+        if (raycastDownGood > 0) //Si en bas ca touche le mur
+        {
+            if (raycastUpGood > 0) //La je touche le mur en haut
+                return false;
+            else    //La je touche pas le mur en haut
+            {
+                Debug.Log("Good here");
+                return true;
+            }
+        }
+        else //La je touche pas le mur en bas
+            return false;
+
+
+    }
+
+    private void Climb()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        Vector3 forceToAdd = (Vector3.forward + Vector3.up) * forceClimb;
+
+        rb.AddForce(forceToAdd, ForceMode.Impulse);
+        Debug.Log("Here lets go");
+    }
+
+
+}
---
+++ Assets/Scripts/Player Scripts/PlayerInteractor.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerInteractor : MonoBehaviour
+{
+    public Transform interactionPoint;
+    public float radius;
+    public LayerMask interactableLayer;
+    public int interactableCount;
+
+    [Header("Composant")]
+    private Collider[] colliders = new Collider[5];
+    private Interactable interactable;
+
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        playerInput = this.GetComponent<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        if (playerInput.CanInteract)
+            Detector();
+    }
+
+    /// <summary>
+    /// Détecte la présence d'interactable autour du joueurs
+    /// </summary>
+    public void Detector()
+    {
+        interactableCount = Physics.OverlapSphereNonAlloc(interactionPoint.position, radius, colliders, interactableLayer);
+
+        // On Interagis
+        if (interactableCount > 0)
+        {
+            interactable = NearestCollider(colliders);
+
+            if (!interactable) //Sécurité au cas ou
+            {
+                interactable.TextInfo();
+                interactable.Interact();
+            }
+        }
+
+        playerInput.CanInteract = false;
+        interactable = null;
+    }
+
+    /// <summary>
+    /// Récupére Le collider le plus proche du joueurs selon un radius et un layers spécifique
+    /// </summary>
+    /// <param name="cols"></param>
+    /// <returns></returns>
+    private Interactable NearestCollider(Collider[] cols)
+    {
+        float nearestInteractable = 9999;
+        Collider nearestCol = null;
+
+        foreach (Collider col in cols)
+        {
+            if (col == null) continue;
+
+            float currentDistance = Vector3.Distance(col.transform.position, transform.position);
+            if (currentDistance <= nearestInteractable)
+            {
+                nearestInteractable = currentDistance;
+                nearestCol = col;
+            }
+        }
+        return nearestCol.GetComponent<Interactable>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(interactionPoint.position, radius);
+    }
+}
