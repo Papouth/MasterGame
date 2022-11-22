4ce64072fc2497b21eb1bee538dcfe4458294a4f
Dupas Louis <tawairpofficiel@gmail.com>
2022-11-22 14:16:59 +0100

    GetStats
    code du get stat, lorsque l'on interagis avec l'objet qui nous donnera une nouvelle compétence, on pourra jouer l'anim du joueur qui la récupère + ajouter la compétence au joueur

--- Assets/Scripts/Objects/GetStat.cs
+++ Assets/Scripts/Objects/GetStat.cs
@@ -3,7 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class GetStat : MonoBehaviour
+public class GetStat : Interactable
 {
-    public UnityEvent leverEvent;
+    public UnityEvent statEvent;
+    private bool statState;
+    public Animation statAnim;
+
+
+    public override bool Interact()
+    {
+        //Debug.Log(onInteractText);
+
+        if (!statState) statEvent.Invoke();
+
+        return true;
+    }
+
+    public void AnimSuperForce()
+    {
+        // On joue l'animation de récupération de la super force
+        statState = true;
+        //statAnim.Play();
+    }
+
+    public override void GoToHand(GameObject hands, PlayerInput playerInput)
+    {
+
+    }
 }
